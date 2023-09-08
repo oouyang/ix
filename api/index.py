46a3149a3223ff0658fc7b93c9a69f5994af663b
Your Name <you@example.com>
2023-09-08 05:53:16 +0000

    add router

--- api/index.py
+++ api/index.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/l/<string:l>/c/<string:c>')
+def ix():
+  l=request.args.get('l')
+  c=request.args.get('c')
+  return '%d-%d'%(l,c)
+
 @app.route('/')
 def home():
     return 'Hello, World!'
