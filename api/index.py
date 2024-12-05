f656952693a80c8db52575a7a2394ab74da04485
Owen Ouyang <oouyang@users.noreply.github.com>
2024-12-05 19:17:51 +0000

    sub

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ pdir = os.path.dirname(os.path.realpath(__file__))
 pdir = os.path.dirname(pdir)
 sys.path.append(pdir)
 
-from app import getChapter
+from app import getChapter, getCList
 
 #x = getChapter(317,66236)
 
@@ -17,11 +17,18 @@ def ix(l,c):
   #l=request.args.get('l')
   #c=request.args.get('c')
   #ret = '%d-%d'%(l,c)
-  return '<pre>'+'\n'.join(getChapter(c,l))+'</pre>'
+  lst = getCList(l)
+  n = len(lst)
+  nav = ('' if c == 0 else ('<a href=\'/l/%d/c/%d\'>prev</a>' % (l, c-1)) ) + ' ' + ('' if c == n-1 else ('<a href=\'/l/%d/c/%d\'>next</a>' % (l, c+1)))
+  return nav + '<br /><pre>'+'\n'.join(getChapter(c,l))+'</pre><br />' + nav
 
 @app.route('/api/l/<int:l>/c/<int:c>')
 def ixapi(l,c):
   return json.dumps(getChapter(c,l))
+
+@app.route('/api/l/<int:l>')
+def ixl(l):
+  return getCList(l)
 
 @app.route('/')
 def home():
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import requests
 import bs4
+import json
 import urllib3
 urllib3.disable_warnings()
 
 bu = 'https://ixdzs.tw'
+novelList = '/novel/clist/'
 def httpget(u):
     response = requests.request('GET', u, verify=False)
     return response.text
@@ -12,3 +14,11 @@ def getChapter(c, bid):
     text = httpget(url)
     soup = bs4.BeautifulSoup(text,features="html.parser")
     return [i.get_text() for i in soup.select('section p')]
+def getCList(bid):
+    payload = 'bid=%d'%bid
+    url = bu + novelList
+    headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    response = requests.request('POST', url, headers=headers, data=payload, verify=False)
+    return json.loads(response.text)
